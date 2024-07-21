b41fa19db96576dd0f12570d1f5907b6963962f5
luan santos <aluncard6@gmail.com>
2024-07-21 09:24:40 -0300

    tech section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+	return <div>Works</div>;
+};
 
-export default Works
+export default Works;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+import CavasLoader from "./../Loader";
 
-export default Computers
+const Computers = ({ isMobile }) => {
+	const computer = useGLTF("./desktop_pc/scene.gltf");
+	console.log(isMobile);
+	return (
+		<mesh>
+			<hemisphereLight
+				intensity={Math.PI * (70 / 100)}
+				groundColor={"black"}
+			/>
+
+			<pointLight intensity={1} />
+			<spotLight
+				position={[-20, 50, 10]}
+				angle={Math.PI * (12 / 100)}
+				penumbra={1}
+				intensity={1}
+				castShadow={true}
+				shadow-mapSize={1024}
+			/>
+			<primitive
+				object={computer.scene}
+				scale={isMobile ? 0.7 : 0.75}
+				position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+				rotation={[-0.01, -0.2, -0.1]}
+			/>
+		</mesh>
+	);
+};
+
+const ComputerCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 500px)");
+		setIsMobile(mediaQuery.matches);
+
+		const handleMediaQuaryChange = (e) => {
+			setIsMobile(e.matches);
+		};
+
+		mediaQuery.addEventListener("change", handleMediaQuaryChange);
+
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQuaryChange);
+		};
+	}, []);
+	return (
+		<Canvas
+			frameloop="demand "
+			shadows
+			camera={{ position: [25, 3, 5], fov: 25 }}
+			gl={{ preserveDrawingBuffer: true }}
+		>
+			<Suspense fallback={<CavasLoader />}>
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+				/>
+				<Computers isMobile={isMobile} />
+			</Suspense>
+			<Preload all />
+		</Canvas>
+	);
+};
+
+export default ComputerCanvas;
