37922f9020fd652f1f263b170d01f9080d357977
luan santos <aluncard6@gmail.com>
2024-07-30 15:18:35 -0300

    feedbakc section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,8 +8,97 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+
+
+
+const ProjectCard = ({
+	index,
+	name,
+	description,
+	tags,
+	image,
+	source_code_link,
+}) => {
+	return (
+		<motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+			<Tilt
+				options={{
+					max: 45,
+					scale: 1,
+					speed: 450,
+				}}
+				className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+			>
+				<div className="relative w-full h-[230px]">
+					<img
+						src={image}
+						alt={name}
+						className="w-full h-full object-cover rounded-2xl"
+					/>
+					<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+						<div
+							onClick={() =>
+								window.open(source_code_link, "blank")
+							}
+							className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+						>
+							<img
+								src={github}
+								alt="github"
+								className="w-1/2 h-1/2 object-contain"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="mt-5">
+					<h3 className="text-white font-bold tex-[24px] ">{name}</h3>
+					<p className="mt-2 text-secondary">{description}</p>
+				</div>
+				<div className="mt-4 flex flex-wrap gap-2">
+					{tags.map((tag, index) => (
+						<p
+							key={tag.name}
+							className={`text-[14px] ${tag.color}`}
+						>
+							#{tag.name}
+						</p>
+					))}
+				</div>
+			</Tilt>
+		</motion.div>
+	);
+};
 const Works = () => {
-	return <div>Works</div>;
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>meus projetos</p>
+				<h2 className={styles.sectionHeadText}>Projetos.</h2>
+			</motion.div>
+			<div className="w-full flex">
+				<motion.p
+					className="text-secondary text-[17px] max-2-3xl leading-[30px]"
+					variants={fadeIn("", "", 0.1, 1)}
+				>
+					Os projetos a seguir demonstram minhas habilidades através
+					de exemplos do meu trabalho. Cada projeto é descrito com
+					links para o repositorio de código contendo demonstrações.
+					Isto reflete minhas habilidades para resolver problemas
+					complexos, trabalhar com diferentes tecnologias e gerenciar
+					projetos com eficiência
+				</motion.p>
+			</div>
+			<div className="mt-20 flex flex-wrap gap-7">
+				{projects.map((project, index) => (
+					<ProjectCard
+						key={`project-${index}`}
+						{...project}
+						index={index}
+					/>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default SectionWrapper(Works, "works");
